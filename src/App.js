23862432449258526f18ee657259a6f8dfb17e3c
seisen <jochim6.2@hotmail.fr>
2020-12-11 17:07:08 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
-
+import Iter_rec from './iter_rec';
 import './App.css';
 
 function App() {
   return (
-      <div></div>
+      <div>aaaaaaaaaaaaaaa
+        <Iter_rec></Iter_rec>
+      </div>
   );
 }
 
